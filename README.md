dacad1f8078dac53646e770e38281df0131409d1
Mohamed Saleem <saleemilahi21@gmail.com>
2021-08-14 09:23:44 +0530

    Completed with IP Address Tracker Project

---
+++ README.md
@@ -0,0 +1,25 @@
+
+# IMPORT NOTE : ALWAYS OPEN WITH ANY LIVE SERVER 
+
+# Frontend Mentor - IP address tracker solution
+
+This is a solution to the [IP address tracker challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/ip-address-tracker-I8-0yYAH0). Frontend Mentor challenges help you improve your coding skills by building realistic projects. 
+
+# Overview
+
+This is a IP Address Tracker. 
+It will Search location using [IP Geolocation API by IPify](https://geo.ipify.org/) and
+For Map Visualization in usess [LeafletJS](https://leafletjs.com/) [Mapbox](https://www.mapbox.com/) Map. 
+For Mapbox map you must required Token (https://account.mapbox.com/access-tokens/).
+
+# ScreenShot
+
+![](img/screenshort/img-1.png)
+![](img/screenshort/img-2.png)
+![](img/screenshort/img-3.png)
+
+# Built with
+
+- Semantic HTML5 markup
+- Sass(scss)
+- Tired using NPM(node package manager)
--- js/index.js
+++ js/index.js
@@ -29,4 +29,34 @@ const addressInfo = res => {
     location.innerHTML = `${res.location.region} , ${res.location.city} , ${res.location.postalCode}`
     timezone.innerHTML = `UTC${res.location.timezone}`
     isp.innerHTML = res.isp
+
+    LeafletMap(res.location.lat,res.location.lng);
+}
+
+// Leaflet Map
+const LeafletMap = (lat,lang) => {
+    const token = 'pk.eyJ1Ijoic2FsZWVtMjUyNDU3IiwiYSI6ImNrczl3cjdoNjB2OWwyb2x1Y3I1NTdwZjEifQ.Fr5kBv8AY20KhMITH_qC3g';
+    var mymap = L.map('map').setView([lat,lang], 14)
+
+    var container = L.DomUtil.get('map');
+      if(container != null){
+        container._leaflet_id = null;
+      }
+
+    L.tileLayer(`https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=${token}`, {
+        maxZoom: 18,
+        id: 'mapbox/streets-v11',
+        tileSize: 512,
+        zoomOffset: -1
+    }).addTo(mymap);
+
+    const icon = L.icon({
+        iconUrl: '../img/icon-location.svg',
+        iconSize: [25,35]
+    })
+
+    L.marker([lat,lang],{
+        icon
+    }).addTo(mymap)
+
 }
